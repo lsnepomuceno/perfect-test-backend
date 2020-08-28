431e75add7b3459ac9a3ffffc74f8ddfae73ce5d
lsnepomuceno <lsn.nepomuceno@gmail.com>
2020-08-27 21:52:52 -0300

    Assets: Bootstrap validation, product image upload viewer

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,28 +1,47 @@
 <html lang='pt-br'>
+
 <head>
     <meta name="viewport" content="initial-scale=1, maximum-scale=1">
     <title>Perfect Pay - Teste - @yield('title')</title>
-    <link href="{{ asset('/images/brand/favicon.png') }}" rel="icon" type="image/png"/>
+    <link href="{{ asset('/images/brand/favicon.png') }}" rel="icon" type="image/png" />
     <link rel='stylesheet' href="{{ url('/css/app.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&display=swap" rel="stylesheet">
     <style>
-        .wrapper #wrapperContent, .wrapper #wrapperContent.closed {
+        .custom-file-label::after {
+            content: 'Buscar'
+        }
+
+        .wrapper #wrapperContent,
+        .wrapper #wrapperContent.closed {
             padding: 0;
         }
     </style>
 </head>
+
 <body>
-<!-- NAVBAR TOP -->
-@include('layout_header')
-<div class='wrapper'>
-    <div id='wrapperContent' class='content container-fluid'>
-        <div id='main'>
+    <!-- NAVBAR TOP -->
+    @include('layout_header')
+    <div class='wrapper'>
+        <div id='wrapperContent' class='content container mt-3'>
             @yield('content')
         </div>
     </div>
-</div>
-<script src="{{ url('/js/app.js') }}"></script>
-<script src="https://kit.fontawesome.com/d712964458.js" crossorigin="anonymous"></script>
-@yield('script')
+    <script src="{{ url('/js/app.js') }}"></script>
+    <script src="{{ url('/js/bootstrap-validation.js') }}"></script>
+
+    {{-- Incluso apenas para exibição da magem no momento do cadastro do produto --}}
+    <script>
+        document.querySelector('#image').onchange = (event) => {
+            document.querySelector('label[for="image"]').innerText = event.target.files[0].name
+            const reader = new FileReader()
+            reader.readAsDataURL(event.target.files[0])
+            reader.onload = () => {
+                document.querySelector('#productThumb').src = reader.result
+            }
+        }
+    </script>
+    <script src="https://kit.fontawesome.com/d712964458.js" crossorigin="anonymous"></script>
+    @yield('script')
 </body>
+
 </html>
